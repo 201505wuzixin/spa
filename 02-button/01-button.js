9f978ed656e69e4fb121483fad8445ad1beb1170
吴子鑫 <1123116007@qq.com>
2018-03-21 18:13:59 +0800

    3.21

---
+++ 02-button/01-button.js
@@ -0,0 +1,31 @@
+var timerButton=function(config){
+	var $btn=$('<input id="wd-timer">')
+}
+$(function(){
+	var $btn=$('.main input'),
+	timer;
+	t=9;
+	
+	disableBtn();
+	
+	$btn.click(function(){
+		alert(1);
+	})
+	function enableBtn(){
+		$btn.val('同意');
+		$btn.removeAttr('disabled');
+	}
+	function disableBtn(){
+		$btn.attr('disabled','disabled')
+		.val('同意('+t+'s)');
+		timer=window.setInterval(function(){
+			t--;
+			if(t===0){
+				enableBtn();
+				window.clearInterval(timer);
+				return;
+			}
+			$btn.val('同意('+t+'s)');
+		},1000);
+	}
+})
