af6659ebc45fb8e58885ae6cdadd0914f99278de
201505wuzixin <1123116007@qq.com>
2018-03-25 16:56:23 +0800

    init commit

---
+++ password/01-index.js
@@ -0,0 +1,16 @@
+$(function(){
+  var $btnAddPwd=$('#add-password'),
+$btnGetPwd=$('#get-password'),
+pwdTextboxs=[];
+
+$btnAddPwd.click(function(){
+  var pwd=new PasswordTextbox({'container':'div.main'});
+  pwdTextboxs.push(pwd);
+  $btnGetPwd.removeAttr('disabled');
+});
+$btnGetPwd.click(function(){
+  pwdTextboxs.forEach(function(pwd){
+    alert(pwd.getPwd());
+  });
+});
+});
---
+++ password01.js
@@ -0,0 +1,12 @@
+$(function(){
+  $btnAddPwd=$('#add-pwd'),
+$btnGetPwd=$('#get-pwd'),
+pwdArray
+$btnAddPwd.click(function(){
+
+});
+$btnGetPwd.click(function(){
+
+});
+
+})
